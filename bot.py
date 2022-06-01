5e065df69258b0421082658aa6e837ed5602daad
Geborgen <88332109+Geborgen@users.noreply.github.com>
2022-06-01 15:21:29 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -75,7 +75,7 @@ class ModlinkBot(commands.Bot):
             return
 
     async def on_ready(self):
-        print('We have logged in as {0.user}'.format(self))
+        print('Logged in as {0.user}'.format(self))
 
     async def on_message(self, message):
         if message.author == self.user:
